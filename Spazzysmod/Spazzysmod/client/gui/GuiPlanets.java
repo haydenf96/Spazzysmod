d6acf7080a99bb1dfc2a11eeec6ccca40e0cfa50
gegy1000 <gegy1000@gmail.com>
2013-08-28 13:31:11 +0200

    Fixed some imports

--- Spazzysmod/Spazzysmod/client/gui/GuiPlanets.java
+++ Spazzysmod/Spazzysmod/client/gui/GuiPlanets.java
@@ -5,7 +5,7 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.StringTranslate;
-import Spazzysmod.Spazzysmod;
+import Spazzysmod.SpazzysmodBase;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
