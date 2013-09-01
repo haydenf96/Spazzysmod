b895f013de7c96f8b3087ce04263e7c8034d2232
MrMewniverse <mewmewniverse@gmail.com>
2013-09-01 15:59:24 +1000

    Trying to fix this weird glitch...

--- spazzysmod/SpazzysResources.java
+++
@@ -1,5 +0,0 @@
-package spazzysmod;
-
-public class SpazzysResources {
-
-}
--- spazzysmod/planets/SpazzysPlanets.java
+++
@@ -1,7 +0,0 @@
-package spazzysmod.planets;
-
-public class SpazzysPlanets {
-
-	public static final Planet MOON = new PlanetMoon ();
-	public static final Planet MARS = new PlanetMars ();
-}
