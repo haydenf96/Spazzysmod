1e6c8f1eb52cc2d57c4b69c0782dcbf76b7e1d32
MrMewniverse <mewmewniverse@gmail.com>
2013-09-04 16:28:10 +1000

    Fixed the launch pad so that it actually works how I want it to...

--- spazzysmod/item/ItemLaunchpad.java
+++ spazzysmod/item/ItemLaunchpad.java
@@ -14,17 +14,17 @@ public class ItemLaunchpad extends Item {
 		super ( par1 );
 		this.setCreativeTab ( SpazzysTabs.tabSolarSystem );
 		this.setMaxDamage ( 8 );
-		this.func_111206_d ( SpazzysmodBase.MODID + ":launchPad.png" );
+		this.setMaxStackSize ( 1 );
+		this.func_111206_d ( SpazzysmodBase.MODID + ":launchPad" );
 	}
 
 	@Override
 	public boolean onItemUse ( ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10 ) {
-		int itemDamage = par1ItemStack.getItemDamage();
-
 		if ( par3World.getBlockId ( par4, par5, par6 ) == SpazzysBlocks.launchPad.blockID ) {
-			this.setDamage ( par1ItemStack, itemDamage + 1 );
+			this.setDamage ( par1ItemStack, par1ItemStack.getItemDamage() - 1 );
+			par3World.setBlockToAir ( par4, par5, par6 );
 
-			return false;
+			return true;
 		} else {
 			par3World.setBlock ( par4, par5 + 1, par6, SpazzysBlocks.launchPad.blockID );
 			par1ItemStack.damageItem ( 1, par2EntityPlayer );
