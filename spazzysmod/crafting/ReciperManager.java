4c85a425db08cc0925f80ab699708f307601f6d0
gegy1000 <gegy1000@gmail.com>
2013-09-05 10:58:24 +0200

    Added recipe for rocket workbench

--- spazzysmod/crafting/ReciperManager.java
+++ spazzysmod/crafting/ReciperManager.java
@@ -11,57 +11,65 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ReciperManager {
 
-    public static void craftItems () {
-        GameRegistry.addRecipe(new ItemStack(MoonBlocks.cheeseBlock),
-                new Object[] { "   ", " X ", "   ", 'X', Item.bucketMilk });
-        GameRegistry.addRecipe(new ItemStack(SpazzysItems.titaniumHelmet),
-                new Object[] { "   ", "XXX", "X X", 'X',
-                        SpazzysItems.titaniumIngot, 'Y', Item.diamond });
+	public static void craftItems () {
+		GameRegistry.addRecipe(new ItemStack(MoonBlocks.cheeseBlock),
+				new Object[] { "   ", " X ", "   ", 'X', Item.bucketMilk });
+		GameRegistry.addRecipe(new ItemStack(SpazzysItems.titaniumHelmet),
+				new Object[] { "   ", "XXX", "X X", 'X',
+			SpazzysItems.titaniumIngot, 'Y', Item.diamond });
 
-        GameRegistry.addRecipe(new ItemStack(SpazzysItems.titaniumChestplate),
-                new Object[] { "X X", "XXX", "XXX", 'X',
-                        SpazzysItems.titaniumIngot, 'Y', Item.diamond });
+		GameRegistry.addRecipe(new ItemStack(SpazzysItems.titaniumChestplate),
+				new Object[] { "X X", "XXX", "XXX", 'X',
+			SpazzysItems.titaniumIngot, 'Y', Item.diamond });
 
-        GameRegistry.addRecipe(new ItemStack(SpazzysItems.titaniumLeggings),
-                new Object[] { "XXX", "X X", "X X", 'X',
-                        SpazzysItems.titaniumIngot, 'Y', Item.diamond });
+		GameRegistry.addRecipe(new ItemStack(SpazzysItems.titaniumLeggings),
+				new Object[] { "XXX", "X X", "X X", 'X',
+			SpazzysItems.titaniumIngot, 'Y', Item.diamond });
 
-        GameRegistry.addRecipe(new ItemStack(SpazzysItems.titaniumBoots),
-                new Object[] { "   ", "X X", "X X", 'X',
-                        SpazzysItems.titaniumIngot, 'Y', Item.diamond });
+		GameRegistry.addRecipe(new ItemStack(SpazzysItems.titaniumBoots),
+				new Object[] { "   ", "X X", "X X", 'X',
+			SpazzysItems.titaniumIngot, 'Y', Item.diamond });
 
-        GameRegistry.addRecipe(new ItemStack(SpazzysItems.titaniumSword),
-                new Object[] { " X ", " X ", " Y ", 'X',
-                        SpazzysItems.titaniumIngot, 'Y', Item.diamond });
+		GameRegistry.addRecipe(new ItemStack(SpazzysItems.titaniumSword),
+				new Object[] { " X ", " X ", " Y ", 'X',
+			SpazzysItems.titaniumIngot, 'Y', Item.diamond });
 
-        GameRegistry.addSmelting(SpazzysBlocks.titaniumBlock.blockID,
-                new ItemStack(SpazzysItems.titaniumIngot, 1), 1F);
-        
-        GameRegistry.addRecipe(new ItemStack(SpazzysItems.spaceHelmet),
-                new Object[] { "XXX", "XGX","XXX", 'X',
-                        Block.cloth, 'G', Block.glass });
-        
-        GameRegistry.addRecipe(new ItemStack(SpazzysItems.spaceJacket),
-                new Object[] { "XRX", "XXX","XXX", 'X',
-                        Block.cloth, 'R', Item.redstone });
-        
-        GameRegistry.addRecipe(new ItemStack(SpazzysItems.spacePants),
-                new Object[] { "XXX", "X X","X X", 'X',
-                        Block.cloth});
-        
-        GameRegistry.addRecipe(new ItemStack(SpazzysItems.spaceShoes),
-                new Object[] { "X X","X X", 'X',
-                        Block.cloth});
-        
-        RocketCraftingManager.func_92051_a(new ItemStack(SpazzysItems.spaceship),  new Object[] 
-        		{ 
-        	"  x  ",
-        	" xgx ",
-        	" xxx ",
-        	"  x  ",
-        	" xrx ",
-        	'x',
-            SpazzysItems.titaniumIngot, 'r', SpazzysBlocks.rocketBlock, 'g', Block.glass 
-            });
-    }
+		GameRegistry.addSmelting(SpazzysBlocks.titaniumBlock.blockID,
+				new ItemStack(SpazzysItems.titaniumIngot, 1), 1F);
+
+		GameRegistry.addRecipe(new ItemStack(SpazzysItems.spaceHelmet),
+				new Object[] { "XXX", "XGX","XXX", 'X',
+			Block.cloth, 'G', Block.glass });
+
+		GameRegistry.addRecipe(new ItemStack(SpazzysItems.spaceJacket),
+				new Object[] { "XRX", "XXX","XXX", 'X',
+			Block.cloth, 'R', Item.redstone });
+
+		GameRegistry.addRecipe(new ItemStack(SpazzysItems.spacePants),
+				new Object[] { "XXX", "X X","X X", 'X',
+			Block.cloth});
+
+		GameRegistry.addRecipe(new ItemStack(SpazzysItems.spaceShoes),
+				new Object[] { "X X","X X", 'X',
+			Block.cloth});
+
+		GameRegistry.addRecipe(new ItemStack(SpazzysBlocks.rocketWorkbench),
+				new Object[] { 
+			"XXX",
+			"X*X",
+			"XXX",
+			'*',
+			Block.workbench, 'X', Item.ingotIron});
+
+		RocketCraftingManager.func_92051_a(new ItemStack(SpazzysItems.spaceship),  new Object[] 
+				{ 
+			"  x  ",
+			" xgx ",
+			" xxx ",
+			"  x  ",
+			" xrx ",
+			'x',
+			SpazzysItems.titaniumIngot, 'r', SpazzysBlocks.rocketBlock, 'g', Block.glass 
+				});
+	}
 }
